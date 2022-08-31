9c7156e586017664546ea72b01be6506b8eba16b
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2022-08-31 05:55:03 -0300

    criando interfaces

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+import { VehicleZodSchema } from './IVehicle';
+
+export const carZodSchema = VehicleZodSchema.extend({
+  doorsQty: z.number().gte(2).lte(4),
+  seatsQty: z.number().gte(2).lte(7),
+});
+
+export type ICar = z.infer<typeof carZodSchema>;
